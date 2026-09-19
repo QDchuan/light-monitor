9d394b3fb7c55ff8527b51f5df3c9740339bc2af
qdquanquan <13725673+qdquanquan@user.noreply.gitee.com>
2026-09-19 19:51:30 +0800

    清理临时脚本

--- _fix4.py
+++
@@ -1,5 +0,0 @@
-p = r'C:\Users\chuan\Doubao\chats\2026-09-19\new-chat-1\feishu_bot.py'
-c = open(p, encoding='utf-8').read()
-c = c.replace(', device, device', ', device')
-open(p, 'w', encoding='utf-8').write(c)
-print("done")
